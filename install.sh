980e97c6d9c3c8a60dc73b3c1a5dc4fa87bf957e
Ethan Martin <ethan@ethanmartin.net>
2017-11-25 17:48:06 -0500

    add super hacky bootstrap script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# creates links for all filenames listed in manifest.txt to the home directory
+
+echo "Symlinking all files from 'manifest.txt' to your home folder..."
+# make sure that the manifest exists
+if [ ! -e manifest.txt ]; then
+    echo "manifest.txt not found. Aborting..."
+    exit 1
+fi
+
+# symlink those files, yo
+while read -u 3 f; do
+    # make sure the source file exists
+    if [ ! -e $f ]; then
+        echo "'$f' not found. Continuing..."
+        continue
+    fi
+    
+    # make sure that target does not exist
+    of="$HOME/.$f"
+    if [ -f $of ]; then
+        echo "Removing '$of'..."
+        #rm $of
+    fi
+
+    # create link
+    echo "Creating link from './$f' to '$of'"
+    # ln -s $f $of
+done 3<manifest.txt
