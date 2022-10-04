436f020b5d04b5eef3295b9c5845022bfb8c9b00
Ethan Martin <ethan@emar10.dev>
2022-10-04 18:29:50 -0400

    neovim: Add Rust LSP support

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -133,9 +133,36 @@ cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done {})
 
 
 -- LSP Config
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { noremap = true })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true })
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { noremap = true })
+
+local on_attach = function(client, bufnr)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
 local servers = {
+    "marksman",
+    "rust_analyzer",
     "sumneko_lua",
-    "marksman"
 }
 
 require('mason').setup()
@@ -144,6 +171,14 @@ require('mason-lspconfig').setup({
 })
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require'lspconfig'.marksman.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+require'lspconfig'.rust_analyzer.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
 require'lspconfig'.sumneko_lua.setup {
     settings = {
         Lua = {
@@ -158,9 +193,7 @@ require'lspconfig'.sumneko_lua.setup {
             }
         }
     },
-    capabilities = capabilities
-}
-require'lspconfig'.marksman.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach,
 }
 
