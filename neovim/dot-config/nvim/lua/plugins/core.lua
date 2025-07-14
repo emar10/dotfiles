5049871303baf329cd58029ea33e3de12fa16415
Ethan Martin <ethan@emar10.dev>
2025-07-13 21:13:11 -0400

    neovim: Switch LazyVim branch to support mason 2.0

--- neovim/dot-config/nvim/lua/plugins/core.lua
+++ neovim/dot-config/nvim/lua/plugins/core.lua
@@ -1,6 +1,16 @@
+-- return {
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
 return {
   {
     "LazyVim/LazyVim",
+    url = "https://github.com/dpetka2001/LazyVim",
+    branch = "fix/mason-v2",
     opts = {
       colorscheme = "catppuccin",
     },
