ad2606768837b8cab65978fd9d1c04dd999555e9
Ethan Martin <ethan@emar10.dev>
2026-05-24 10:54:39 -0400

    Add testing neovim config (usable via nvim-test)

---
+++ neovim-testing/dot-config/nvim-test/init.lua
@@ -0,0 +1,31 @@
+vim.loader.enable()
+
+-- options
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.list = true
+vim.o.inccommand = 'nosplit'
+vim.o.cursorline = true
+
+-- keymaps
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- plugins
+vim.pack.add({
+  { src = 'https://github.com/catppuccin/nvim' }
+})
+
+vim.cmd.colorscheme 'catppuccin-nvim'
+
+-- enable experimental ui2
+require('vim._core.ui2').enable({ enable = true, msg = { targets = 'msg' }})
+
+-- vim: ts=2 sts=2 sw=2 et
